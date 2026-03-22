068eeee369d95dc0fa820fbd62c0377d3e82783d
Dimitris Nikolos <dnikolos@gmail.com>
2026-03-22 15:08:30 +0200

    online version

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const hashBuffer = await crypto.subtle.digest("SHA-512", arrayBuffer);
             const hashArray = Array.from(new Uint8Array(hashBuffer));
             const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-            
+
             setProgress(85);
             setStatus("Δημιουργία PDF…", "var(--fg-dim)");
 
